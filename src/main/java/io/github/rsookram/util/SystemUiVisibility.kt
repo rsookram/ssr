60e2ae5257ff2cbb23f22b331e12dd9d3e5b64fe
rsookram <rashadsookram@gmail.com>
2022-07-16 11:44:24 -0400

    Remove direct use of androidx.core

--- src/main/java/io/github/rsookram/util/SystemUiVisibility.kt
+++ src/main/java/io/github/rsookram/util/SystemUiVisibility.kt
@@ -1,16 +1,15 @@
 package io.github.rsookram.util
 
 import android.view.Window
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 
 fun Window.enterImmersiveMode() {
-    WindowCompat.setDecorFitsSystemWindows(this, false)
+    setDecorFitsSystemWindows(false)
 
-    WindowCompat.getInsetsController(this, decorView).apply {
-        systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        hide(WindowInsetsCompat.Type.systemBars())
-    }
+    val insetsController = insetsController ?: return
+
+    insetsController.systemBarsBehavior =
+        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    insetsController.hide(WindowInsets.Type.systemBars())
 }
